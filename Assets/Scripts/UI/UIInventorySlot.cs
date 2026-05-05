5db49152da41130c45444e14eba5b4a34261aa2c
KIM KWANGHWAN <louie1004@naver.com>
2026-05-05 16:51:19 +0900

    UI 설정 확인
    플레이어 레벨업 시 아이템 선택 창 활성화, 아이템 선택시 인벤토리에 등록

--- Assets/Scripts/UI/UIInventorySlot.cs
+++ Assets/Scripts/UI/UIInventorySlot.cs
@@ -11,21 +11,27 @@ public class UIInventorySlot : MonoBehaviour
     {
         for (int i = 0; i < weaponIcons.Length; i++) weaponIcons[weaponCount++].enabled = false;
         for (int i = 0; i < passiveIcons.Length; i++) passiveIcons[passiveCount++].enabled = false;
-        UpgradeManager.Instance.OnFirstGet += InventorySlotUpdate;
+        
         weaponCount = 0;
         passiveCount = 0;
+    }
+    private void Start()
+    {
+        UpgradeManager.Instance.OnFirstGet += InventorySlotUpdate;
     }
     public void InventorySlotUpdate(IUpgrade item)
     {
         if (item.type == UpgradeItemType.Weapon)
         {
-            weaponIcons[weaponCount++].sprite = item.Icon;
-            weaponIcons[weaponCount++].enabled = true;
+            weaponIcons[weaponCount].sprite = item.Icon;
+            weaponIcons[weaponCount].enabled = true;
+            weaponCount++;
         }
         else if (item.type == UpgradeItemType.Passive)
         {
-            passiveIcons[passiveCount++].sprite = item.Icon;
-            passiveIcons[passiveCount++].enabled = true;
+            passiveIcons[passiveCount].sprite = item.Icon;
+            passiveIcons[passiveCount].enabled = true;
+            passiveCount++;
         }
     }
 }
